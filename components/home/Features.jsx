a0c8e68dce62ceb895047155100207b1d9b5c7c9
Saud Kadiri <saudkadiri@gmail.com>
2022-09-15 18:46:40 +0530

    Revert "Image optimisation done"
    This reverts commit ee460135d450f0725906e1e3d3d4956a31eab988.

--- components/home/Features.jsx
+++ components/home/Features.jsx
@@ -1,5 +1,3 @@
-import Image from 'next/image';
-
 export const Features = () => {
   const features = [
     {
@@ -11,7 +9,7 @@ export const Features = () => {
       title: 'Fraud Prevention',
     },
     {
-      icon: '/nft.svg',
+      icon: './nft.svg',
       title: 'NFT Based Warranties',
     },
   ];
@@ -24,12 +22,10 @@ export const Features = () => {
             className="flex gap-4 items-center justify-center even:border-x-2 border-white/20"
             key={title}
           >
-            <Image
+            <img
               src={icon}
               alt={title}
               className="w-9 h-9 lg:w-12 lg:h-12 xl:w-14 xl:h-14"
-              height={60}
-              width={60}
             />
             <h3 className="text-sm lg:text-base xl:text-lg">{title}</h3>
           </div>
