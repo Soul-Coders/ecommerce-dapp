881380d14021973bfa6c03ae83b8c4b8a6333f44
Saud Kadiri <saudkadiri@gmail.com>
2022-09-24 14:34:51 +0530

    Cleaning the last commit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -33,7 +33,7 @@ export default class Navbar extends Component {
         <Link href="/">
           <img
             src="./user.png"
-            alt="Netflix profile icon blue"
+            alt="User profile icon"
             className="cursor-pointer rounded"
             width={30}
             height={30}
@@ -44,48 +44,3 @@ export default class Navbar extends Component {
     )
   }
 }
-
-
-
-// function Navbar(props) {
-//   const handleClick = (event) => {
-//     console.log('List button clicked')
-//     console.log(this.props)
-//   };
-//   return (
-//     <header className="bg-dimmed-black flex justify-between shadow-md">
-//       {/* Logo and the sidebar icon */}
-//       <div className=" flex wrap items-centre space-x-10 md:space-x-10 p-3">
-//         <Link href={'/'}>
-//           <img
-//             src="/logo.svg"
-//             width={150}
-//             height={150}
-//             alt="Shoppingverse Logo"
-//             className="cursor-pointer"
-//           />
-//         </Link>
-//         <button onClick={handleClick}>
-//           <ListBulletIcon className="h-6 w-6 sm:inline cursor-pointer" />
-//         </button>
-//       </div>
-
-//       {/* FOR THE ICONS IN RHS */}
-//       <div className="flex items-center space-x-4 font-light p-3">
-//         <SunIcon className="hidden h-6 w-6 sm:inline" />
-//         <BellIcon className="hidden h-6 w-6 sm:inline" />
-//         <Link href="/">
-//           <img
-//             src="./user.png"
-//             alt="Netflix profile icon blue"
-//             className="cursor-pointer rounded"
-//             width={30}
-//             height={30}
-//           />
-//         </Link>
-//       </div>
-//     </header>
-//   );
-// }
-
-// export default Navbar;
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -10,7 +10,6 @@ import {
 
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react'
 
 const sidebarItems = [
   {
