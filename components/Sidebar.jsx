8409bb5bbeb25ece385918e3dfeba6a18e1aaa1c
Saud Kadiri <saudkadiri@gmail.com>
2022-09-28 10:57:21 +0530

    Changed Navbar and sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { XCircleIcon } from '@heroicons/react/20/solid';
+import { XCircleIcon, SunIcon, BellIcon
+ } from '@heroicons/react/20/solid';
 
 import seller_sidebar from '../utils/seller_sidebar';
 import buyer_sidebar from '../utils/buyer_sidebar';
@@ -16,19 +17,33 @@ const Sidebar = ({ onClick, collapsed }) => {
     <div>
       <div className="backdrop-blur-sm bg-black/30 flex flex-col justify-between text-brand-lavender bg-dimmed-black w-64 fixed top-0 left-0 h-full z-10">
         <div>
-          <div className="flex justify-between px-2 py-5">
-            <img src="/logo.svg" alt="logo" className="w-36 lg:w-40" />
+          <div className='flex justify-between  px-2 py-5'>
+            <div className="flex items-center space-x-4 font-light p-3">
+              <Link href="/">
+                <img
+                  src="/user.png"
+                  alt="User profile icon"
+                  className="cursor-pointer rounded"
+                  width={30}
+                  height={30}
+                />
+              </Link>
+              <BellIcon className="h-6 w-6 sm:inline" />
+              <SunIcon className="h-6 w-6 sm:inline" />
+            </div>
             <button onClick={onClick}>
               <XCircleIcon className="h-7 w-7 sm:inline cursor-pointer" />
             </button>
-          </div>
+            
+          </div>          
+
           <ul>
             {sidebarItems.above.map(({ icon, title, id }) => (
               <li key={title} className="p-2">
                 <Link href={`${parent}/${id}`}>
                   <div
                     className={`${
-                      child == id && 'bg-[#242323] rounded-md'
+                      child == id && 'bg-[#242323]/80 rounded-md'
                     } p-3 hover:bg-blue-500 rounded-md`}
                   >
                     <div className="flex flex-wrap gap-2 w-fit h-fit cursor-pointer">
