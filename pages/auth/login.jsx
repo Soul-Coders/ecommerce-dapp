717dc7fe0e4cc85e3ce4f1e114b2a2739b30b77f
Farhaan Khan <farhaankhanfiroz@gmail.com>
2022-09-28 17:33:17 +0530

    Tweak login page

--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,8 +1,8 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import { useContext } from 'react';
 import { Button } from '../../components/Button';
 import { ConnectionContext } from '../../context/ConnectionContext';
-import Link from 'next/link';
 
 const Login = () => {
   const { connectSeller, connectBuyer } = useContext(ConnectionContext);
@@ -21,7 +21,7 @@ const Login = () => {
       <main>
         <div className="grid grid-cols-1 md:grid-cols-[40%_60%]">
           <div className="flex w-full h-screen items-center justify-center">
-            <div className="w-min flex flex-col justify-between">
+            <div className="w-min grid grid-rows-[min-content_1fr] h-full py-10 justify-between">
               <div className="flex w-full justify-center">
                 <div className="w-44 lg:w-56">
                   <Link href={'/'}>
@@ -33,22 +33,24 @@ const Login = () => {
                   </Link>
                 </div>
               </div>
-              <h1 className="text-4xl text-center w-[10ch] font-semibold tracking-tight leading-[1.3] lg:text-5xl lg:leading-[1.2] mt-14">
-                Welcome to the new era of e-commerce
-              </h1>
-              <div className="flex flex-col gap-5 mt-14">
-                <Button
-                  onClick={connectBuyer}
-                  className="bg-white text-black py-3 font-bold lg:py-3.5"
-                >
-                  Login as Buyer
-                </Button>
-                <Button
-                  onClick={connectSeller}
-                  className="bg-background border border-white py-3 font-medium lg:py-3.5"
-                >
-                  Login as Seller
-                </Button>
+              <div className="flex flex-col gap-10 justify-center lg:gap-16">
+                <h1 className="text-4xl text-center w-[10ch] font-semibold tracking-tight leading-[1.3] lg:text-5xl lg:leading-[1.2]">
+                  Welcome to the new era of e-commerce
+                </h1>
+                <div className="flex flex-col gap-5">
+                  <Button
+                    onClick={connectBuyer}
+                    className="bg-white text-black py-3 font-bold lg:py-3.5"
+                  >
+                    Login as Buyer
+                  </Button>
+                  <Button
+                    onClick={connectSeller}
+                    className="bg-background border border-white py-3 font-medium lg:py-3.5"
+                  >
+                    Login as Seller
+                  </Button>
+                </div>
               </div>
             </div>
           </div>
