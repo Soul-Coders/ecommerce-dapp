c5a79044bdc4c0eb6854fff3e3d8aeb0b4327283
unknown <anaskhan91315@gmail.com>
2022-09-30 20:48:55 +0530

    Dashboard done

--- pages/seller/dashboard.jsx
+++ pages/seller/dashboard.jsx
@@ -1,161 +1,73 @@
 import React from 'react';
 import Page from '../../components/Page';
+import Order from '../../components/Order';
 
 const Dashboard = () => {
   return (
     <div>
       <Page name={'Dashboard'} parent="seller">
         <div>
-          <div className="grid grid-cols-1 md:grid-cols-3">
-            <div className="p-4">
-              <div className="grid grid-cols-3 md:grid-cols-2 border rounded-md bg-[#181818]">
+          <div className="grid gap-5 mb-5 grid-cols-1 md:grid-cols-3">
+            <div className="px-3 py-4 flex gap-4 rounded-md bg-dimmed-black">
+              <div className="p-4 flex justify-center bg-[#FF8500]/10 rounded-full w-fit">
                 <img
                   src="/rupee.svg"
                   alt="dollar"
-                  className="w-5/12 md:w-6/12 lg:w-5/12 justify-center ml-7 p-2.5"
+                  className="text-orange-600 w-6"
                 />
-                <div className="flex flex-col justify-center text-bold text-md md:text-sm lg:text-md">
-                  <h1>Sales</h1>
-                  <h1>$19,616,051.20</h1>
-                </div>
+              </div>
+              <div className="flex lg:gap-1 flex-col justify-center text-bold text-md md:text-sm lg:text-md">
+                <h1 className="text-sm text-white/50">Total Sales</h1>
+                <h1 className="font-semibold md:text-lg">$19,616,051.20</h1>
               </div>
             </div>
-            <div className="p-4">
-              <div className="grid grid-cols-3 md:grid-cols-2 border rounded-md bg-[#181818]">
+            <div className="px-3 py-4 flex gap-4 rounded-md bg-dimmed-black">
+              <div className="p-4 flex justify-center bg-[#00B517]/10 rounded-full w-fit">
                 <img
                   src="/cart.svg"
                   alt="cart"
-                  className="w-5/12 md:w-6/12 lg:w-5/12 justify-center ml-7 p-2.5"
+                  className="text-orange-600 w-6"
                 />
-                <div className="flex flex-col justify-center text-bold text-md md:text-sm lg:text-md">
-                  <h1>Total Orders</h1>
-                  <h1>3290</h1>
-                </div>
+              </div>
+              <div className="flex lg:gap-1 flex-col justify-center text-bold text-md md:text-sm lg:text-md">
+                <h1 className="text-sm text-white/50">Total Orders</h1>
+                <h1 className="md:text-lg font-semibold">3290</h1>
               </div>
             </div>
-            <div className="p-4">
-              <div className="grid grid-cols-3 md:grid-cols-2 border rounded-md bg-[#181818]">
+            <div className="px-3 py-4 flex gap-4 rounded-md bg-dimmed-black">
+              <div className="p-4 flex justify-center bg-[#3167EB]/10 rounded-full w-fit">
                 <img
                   src="/basket.svg"
-                  alt="cart"
-                  className="w-5/12 md:w-6/12 lg:w-5/12 justify-center ml-7 p-2.5"
+                  alt="basket"
+                  className="text-orange-600 w-6"
                 />
-                <div className="flex flex-col justify-center text-bold text-md md:text-sm lg:text-md">
-                  <h1>Total Products</h1>
-                  <h1>332</h1>
-                </div>
+              </div>
+              <div className="flex lg:gap-1 flex-col justify-center text-bold text-md md:text-sm lg:text-md">
+                <h1 className="text-sm text-white/50">Total Products</h1>
+                <h1 className="md:text-lg font-semibold">332</h1>
               </div>
             </div>
           </div>
-          <div className="grid md:grid-cols-12">
-            <div className="md:col-span-8 ">
+          <div className="grid grid-cols-1 md:grid-cols-[60%_40%]">
+            <div className="flex">
               <img
                 src="/barChart.svg"
                 alt="Bar"
-                className="object-fill w-full h-full md:py-4 md:w-11/12"
+                className="object-cover object-center"
               />
             </div>
-            <div className="md:col-span-4 w-full h-full">
+            <div className="flex">
               <img
                 src="/pieChart.svg"
                 alt="Pie"
-                className="h-full w-full md:object-fill"
+                className="object-contain object-center"
               />
             </div>
           </div>
           <div className="grid p-1">
             <h1 className="p-3 text-xl font-bold lg:text-2xl">Latest Orders</h1>
-            <div className="flex flex-col-reverse">
-              <div className="grid grid-cols-2 md:grid-cols-4 p-5">
-                <div className="grid grid-cols-2">
-                  <h2>2323</h2>
-                  <h2 className="">Devon Lane</h2>
-                </div>
-                <h2>devon@gmail.com</h2>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>$778.35</h2>
-                  <h2>Delivered</h2>
-                </div>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>07.05.2020</h2>
-                  <h2>...</h2>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 md:grid-cols-4 p-5">
-                <div className="grid grid-cols-2">
-                  <h2>2323</h2>
-                  <h2 className="">Devon Lane</h2>
-                </div>
-                <h2>devon@gmail.com</h2>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>$778.35</h2>
-                  <h2>Delivered</h2>
-                </div>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>07.05.2020</h2>
-                  <h2>...</h2>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 md:grid-cols-4 p-5">
-                <div className="grid grid-cols-2">
-                  <h2>2323</h2>
-                  <h2 className="">Devon Lane</h2>
-                </div>
-                <h2>devon@gmail.com</h2>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>$778.35</h2>
-                  <h2>Delivered</h2>
-                </div>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>07.05.2020</h2>
-                  <h2>...</h2>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 md:grid-cols-4 p-5">
-                <div className="grid grid-cols-2">
-                  <h2>2323</h2>
-                  <h2 className="">Devon Lane</h2>
-                </div>
-                <h2>devon@gmail.com</h2>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>$778.35</h2>
-                  <h2>Delivered</h2>
-                </div>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>07.05.2020</h2>
-                  <h2>...</h2>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 md:grid-cols-4 p-5">
-                <div className="grid grid-cols-2">
-                  <h2>2323</h2>
-                  <h2 className="">Devon Lane</h2>
-                </div>
-                <h2>devon@gmail.com</h2>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>$778.35</h2>
-                  <h2>Delivered</h2>
-                </div>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>07.05.2020</h2>
-                  <h2>...</h2>
-                </div>
-              </div>
-              <div className="grid grid-cols-2 md:grid-cols-4 p-5">
-                <div className="grid grid-cols-2">
-                  <h2>2323</h2>
-                  <h2 className="">Devon Lane</h2>
-                </div>
-                <h2>devon@gmail.com</h2>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>$778.35</h2>
-                  <h2>Delivered</h2>
-                </div>
-                <div className="hidden md:grid md:grid-cols-2">
-                  <h2>07.05.2020</h2>
-                  <h2>...</h2>
-                </div>
-              </div>
+            <div className="flex flex-col">
+              <Order />
             </div>
           </div>
         </div>
