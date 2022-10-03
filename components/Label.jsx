4ccbdeedcf3c1e477a750fdf1947331e1991c992
Saud Kadiri <saudkadiri@gmail.com>
2022-10-04 01:30:46 +0530

    Fixed label color

--- components/Label.jsx
+++ components/Label.jsx
@@ -1,7 +1,8 @@
 function Label({status}) {
   const color = status == 'pending' && 'orange' || status == 'cancelled' && 'red' || 'green'
+  const box = `border-${color}-400 text-${color}-400 bg-${color}-400/30`
   return (
-    <div className={`w-20 p-1.5 flex justify-center rounded-3xl border border-${color}-400 text-${color}-400 bg-${color}-400/30`}>
+    <div className={`w-20 p-1.5 flex justify-center rounded-3xl border ${box}`}>
       {status}
     </div>
   )
