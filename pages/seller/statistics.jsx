565b8d1b097a44501a9ef7e6c8e3c87564a19840
unknown <anaskhan91315@gmail.com>
2022-10-19 12:55:02 +0530

    Charts for Statistics

--- pages/seller/statistics.jsx
+++ pages/seller/statistics.jsx
@@ -3,17 +3,65 @@ import Sales from '../../components/charts/Sales';
 import PieChart from '../../components/charts/Pie';
 import LineChart from '../../components/charts/Line';
 import Dough from '../../components/charts/Doughnut';
+import { ProductCard } from '../../components/products/ProductCard';
 
 const Statistics = () => {
+  const products = [
+    {
+      img: '/product-1.jpg',
+      name: 'Black fashion bag',
+      description: 'test',
+      price: '2000',
+      id: '321341',
+      rating: 4,
+    },
+    {
+      img: '/product-2.jpg',
+      name: 'Wood leather watch',
+      description: 'test',
+      price: '1000',
+      id: '321342',
+      rating: 1.5,
+    },
+    {
+      img: '/product-3.jpg',
+      name: 'Wireless headphones',
+      description: 'test',
+      price: '1500',
+      id: '321343',
+      rating: 3.5,
+    },
+    {
+      img: '/product-4.jpg',
+      name: 'Blue skate shoes',
+      description: 'test',
+      price: '800',
+      id: '321344',
+      rating: 2.5,
+    },
+  ];
   return (
     <div>
       <Page name="Statistics">
-        <div className="grid grid-cols-1 md:grid-cols-[65%_35%]">
+        <div className="flex flex-col md:flex-row justify-evenly">
           <div className="">
-            <Sales />
-          </div>
-          <div className="pt-3 md:pt-0">
-            <PieChart />
+            {products.map(
+              ({
+                productId,
+                productName,
+                productDescription,
+                productImage,
+                productPriceInr,
+              }) => {
+                <ProductCard
+                  id={productId}
+                  img={productImage}
+                  name={productName}
+                  description={productDescription}
+                  price={productPriceInr}
+                />;
+              }
+            )}
           </div>
           <div className="pt-3 md:pt-5">
             <LineChart />
