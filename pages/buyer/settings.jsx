f369dcb6f57b5bde3898a23e277c3a2b4ff6e087
Farhan Shikalgar <farhan@Farhans-MacBook-Pro.local>
2022-10-31 12:16:39 +0530

    buyers setting page

--- pages/buyer/settings.jsx
+++ pages/buyer/settings.jsx
@@ -1,10 +1,85 @@
 import Page from '../../components/Page';
+import Card from '../../components/Card';
+import Form from '../../components/signup/Form';
+
+import { useContext } from 'react';
+import { ConnectionContext } from '../../context/ConnectionContext';
 
 const Settings = () => {
+  const { currentAccount, updateAccount } = useContext(ConnectionContext);
+  var info = { ...currentAccount.info };
+  const onChange = (e) => {
+    info[e.target.id] = e.target.value;
+  };
+  const setProfileImage = (e) => {
+    const file = e.target.files[0];
+    const rfile = new File([file], 'user.png');
+  };
+  const onSave = () => {
+    updateAccount({
+      imgURL: info.imgURL,
+      name: info.name,
+      email: info.email,
+      phone: info.phone,
+      dob: info.dob,
+      gender: info.gender,
+      addr: info.addr,
+      city: info.city,
+      pinCode: info.pinCode,
+    });
+  };
   return (
-    <div>
-      <Page name={'Settings'}>{/* Main Content Here */}</Page>
-    </div>
+    <Page
+      name="Settings"
+      option={
+        <div className="">
+          <button
+            className="text-white/50 border md:flex justify-center items-center font-semibold 
+        px-3 h-10 bg-grey-900 rounded-md hover:text-white hover:bg-rose-800"
+          >
+            Deactivate my account
+          </button>
+        </div>
+      }
+    >
+      <div className="bg-dimmed-black rounded-md p-3 flex flex-col ">
+        <div className="flex items-center md:items-start flex-col-reverse md:flex-row justify-evenly">
+          <Form isUpdate={true} onChange={onChange} />
+          <Card className={'mt-4 h-full'}>
+            <div className="flex flex-col items-center p-4">
+              <img
+                src={currentAccount.info.imgURL}
+                alt="User profile picture"
+                height={200}
+                width={200}
+                className="pb-4"
+              />
+              <input
+                type={'file'}
+                id="imgupload"
+                onClick={(e) => setProfileImage(e)}
+                className="hidden"
+                accept="image/png, image/jpeg"
+              />
+              <label
+                htmlFor="imgupload"
+                className="text-sm flex cursor-pointer justify-center items-center font-semibold p-2 h-10 bg-gradient-to-r from-brand-red to-brand-purple rounded-md orm-control col-lg-2 col-md-2 col-sm-2"
+              >
+                <a>Change Profile Picture</a>
+              </label>
+            </div>
+          </Card>
+        </div>
+        <div className="flex justify-center items-center">
+          <button
+            className="flex w-full md:w-fit h-10 justify-center items-center font-semibold px-9 bg-gradient-to-r from-brand-red to-brand-purple rounded-md"
+            onClick={onSave}
+          >
+            Save Changes
+          </button>
+        </div>
+      </div>
+    </Page>
   );
 };
 
