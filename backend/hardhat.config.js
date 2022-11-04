64c33ab19964ebbb93405962a5d14366fd25f5ed
Farhaan Khan <farhaankhanfiroz@gmail.com>
2022-11-04 23:42:49 +0530

    Alchemy deployment setup

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,5 +1,6 @@
 require('@nomicfoundation/hardhat-toolbox');
 require('hardhat-contract-sizer');
+require('dotenv').config({ path: __dirname + '/.env' });
 
 task('accounts', 'Prints the list of accounts', async () => {
   const accounts = await ethers.getSigners();
@@ -9,11 +10,18 @@ task('accounts', 'Prints the list of accounts', async () => {
   }
 });
 
+const ALCHEMY_API_KEY = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY;
+const GOERLI_PRIVATE_KEY = process.env.NEXT_PUBLIC_GOERLI_PRIVATE_KEY;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`${GOERLI_PRIVATE_KEY}`],
     },
   },
   solidity: '0.8.17',
