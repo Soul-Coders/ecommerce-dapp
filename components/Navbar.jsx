8e042a4d5e9142bd6d62da6f11c29a09ee85683a
Farhaan Khan <farhaankhanfiroz@gmail.com>
2023-04-28 23:39:35 +0530

    Typo in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Bars3Icon } from '@heroicons/react/20/solid';
 import Link from 'next/link';
-import SearchBar from './Searchbar';
+import SearchBar from './SearchBar';
 
 const Navbar = ({ onClick, page, items, setSearches }) => {
   const search =
